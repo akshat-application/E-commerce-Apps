053911394516953acf181ddc5343533eda945bce
akshat-application <akpapk11@gmail.com>
2024-05-30 15:54:25 +0530

    updated search box

--- app/src/main/java/com/akshat/eCommerce/screen/dashboardUi/DashboardScreen.kt
+++ app/src/main/java/com/akshat/eCommerce/screen/dashboardUi/DashboardScreen.kt
@@ -12,10 +12,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
